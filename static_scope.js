1df85144bfa68568dd933f9d7adb5a865278ae06
Burak Guclu <burak.guclu@tedu.edu.tr>
2025-03-04 18:26:13 +0300

    Static Scope example uploaded

---
+++ static_scope.js
@@ -0,0 +1,16 @@
+function big() {
+  function sub1() {
+    var x = 7;
+    console.log(x);
+    sub2();
+  }
+  function sub2() {
+    var y = x;
+    console.log(y);
+  }
+  var x = 3;
+  console.log(x);
+  sub1();
+}
+
+big();
