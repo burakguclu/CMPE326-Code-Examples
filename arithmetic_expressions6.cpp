743c5d3b3b9333e4a1f7113eba816600b9e818bf
Burak Guclu <burak.guclu@tedu.edu.tr>
2025-05-04 15:17:37 +0300

    Adding new example

---
+++ arithmetic_expressions6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int g = 0;
+int f1(int x)
+{
+    return x + 1;
+}
+int f2(int x)
+{
+    g++;
+    return x + g;
+}
+int main()
+{
+    int x = 2, y = 5;
+    int i = f2(x) + f2(y) * (f2(x) - f2(x));
+    cout << "i=" << i << endl;
+}
