8455c4f566404a97294890c345e4a669cbfd3417
Burak Guclu <burak.guclu@tedu.edu.tr>
2025-06-13 22:37:14 +0300

    Write protection example 2 added

---
+++ write_protection_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void fun(const int &p, int a, int &b)
+{
+    a = p + b;
+    b = a + p;
+}
+int main()
+{
+    int x = 5, y = 10;
+    int ptr = 20;
+    fun(ptr, x, y);
+    cout << "x=" << x << endl;
+    cout << "y=" << y << endl;
+    cout << "ptr=" << ptr << endl;
+}
